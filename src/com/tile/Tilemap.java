77ee735eea7ba14e5dcdf36c499f7ac8b608da8b
Nico Poblete <nlypoblet@gmail.com>
2017-04-24 18:40:20 -0700

    Final commit for now

---
+++ src/com/tile/Tilemap.java
@@ -0,0 +1,19 @@
+package tile;
+
+import org.newdawn.slick.*;
+
+public class Tilemap {
+	private SpriteSheet _tileSheet;
+	private int[][] _tilemap;
+	private int[][] _blocked;
+	private int _tilesize;
+	private int _rows;
+	private int _cols;
+	private int _tileWidth;
+	private int _tileHeight;
+	
+	/*public Tilemap() {
+		
+	}*/
+	
+}
